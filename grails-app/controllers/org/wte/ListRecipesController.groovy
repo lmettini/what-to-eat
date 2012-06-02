7cc8094b71e05ffcc8ae963858b66a0a104e4ce2
Leandro Mettini <lea1987@gmail.com>
2012-06-02 00:44:46 -0300

    validacion x id nulo en listado

--- grails-app/controllers/org/wte/ListRecipesController.groovy
+++ grails-app/controllers/org/wte/ListRecipesController.groovy
@@ -3,10 +3,13 @@ package org.wte
 class ListRecipesController {
 
     def index = {
-        def ingredientsIds = params.id.toString().split(',')
         def components = []
-        ingredientsIds.each {
-            components.addAll(RecipeComponent.findByIngredient(Ingredient.get(it.toInteger())))
+        
+        if(params.id){
+		    def ingredientsIds = params.id.toString().split(',')
+		    ingredientsIds.each {
+		        components.addAll(RecipeComponent.findByIngredient(Ingredient.get(it.toInteger())))
+		    }
         }
 
         ["components":components]
