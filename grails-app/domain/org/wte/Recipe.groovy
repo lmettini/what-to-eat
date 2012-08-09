e2dfaa3c9319cbf5a529e4e7108e26b8ad904737
Martin Caruso <caruso.martin@gmail.com>
2012-08-09 00:10:00 -0300

    recipe fix

--- grails-app/domain/org/wte/Recipe.groovy
+++ grails-app/domain/org/wte/Recipe.groovy
@@ -3,19 +3,21 @@ package org.wte
 class Recipe {
 
     static hasMany = [components:RecipeComponent, likes: UserLikeRecipe]
-
-    String title
-    SecUser user
-    RecipeCategory category
-    String summary
-    String description
+	
+	String name
+/*
+      String title
+      SecUser user
+      RecipeCategory category
+      String summary
+      String description
 
     static constraints = {
-        title blank: false
-        user blank: false
-        category blank: false
+       title blank: false
+       user blank: false
+       category blank: false
     }
-
+*/
 	static searchable = {
 	    components component: true
 	}
