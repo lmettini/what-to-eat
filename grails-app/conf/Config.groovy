53ffcb8c4124809257b3ad852a60e3fa6a8b46b8
Martin Caruso <caruso.martin@gmail.com>
2012-09-29 23:48:42 -0300

    cambio de pass

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -121,7 +121,7 @@ grails {
      host = "smtp.gmail.com"
      port = 465
      username = "zumos.utn@gmail.com"
-     password = "zumos123"
+     password = "zumos.utn"
      props = ["mail.smtp.auth":"true",
               "mail.smtp.socketFactory.port":"465",
               "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
